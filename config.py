c0343e8315c88acf64a03d9f1dd2bed7eb51c102
Bekzod Buyukov <bek.buyukov@gmail.com>
2020-10-13 21:26:14 +0700

    Чуть-чуть рефактиронга конфигов

--- config.py
+++ config.py
@@ -6,12 +6,14 @@ load_dotenv()
 
 # getting values from environment variable
 TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_ID = [1014334488, 593127562]
+ADMIN_ID = os.getenv('ADMIN_ID')
 SUPPORT = os.getenv('SUPPORT_USERNAME')
 BOT_VERSION = os.getenv('BOT_VERSION')
 DB_FILE = os.getenv('DB_FILE_PATH')
 GROUPS_FILE = os.getenv('GROUPS_FILE_PATH')
 PATH_TO_TT_FILES = os.getenv('TIMETABLE_FILES_PATH')
 DOMAIN = os.getenv('DOMAIN_ADDRESS')
-# HEADERS = os.getenv('USER_AGENT_HEADERS')
-HEADERS = {'User-Agent': 'Bot/1.0.0'}
+HEADERS = os.getenv('USER_AGENT_HEADERS')
+
+print(f'{TOKEN}\n{ADMIN_ID}\n{SUPPORT}\n{BOT_VERSION}\n{DB_FILE}\n{GROUPS_FILE}\n{PATH_TO_TT_FILES}\n{DOMAIN}'
+      f'\n{HEADERS}')
